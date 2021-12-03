7307ee68dbdc38738382e4c8194acf628291f118
Virgile <virgile.theuerlacher@worldline.com>
2021-12-03 11:56:04 +0100

    2021 day 3 part 2

--- src/year_2021/day03.rs
+++ src/year_2021/day03.rs
@@ -1,8 +1,41 @@
 use crate::day::Day;
+use fnv::FnvHashSet;
 
 pub struct Day03 {
     numbers: Vec<u16>,
     bit_count: u8,
+}
+
+fn get_bit(number: u16, i: u8) -> u8 {
+    if number & (1 << i) > 0 {
+        1
+    } else {
+        0
+    }
+}
+
+fn count_ones(numbers: &[u16], i: u8) -> usize {
+    numbers.iter().filter(|&&n| get_bit(n, i) == 1).count()
+}
+
+impl Day03 {
+    fn find_by_bit_criteria(&self, keep_most_common: bool) -> usize {
+        (0..self.bit_count).rev().fold(self.numbers.clone(), |candidates, i| {
+            if candidates.len() == 1 {
+                candidates
+            } else {
+                let most_common = if count_ones(&candidates, i) as f32 >= candidates.len() as f32 / 2f32 {
+                    1
+                } else {
+                    0
+                };
+                candidates
+                    .into_iter()
+                    .filter(|&n| keep_most_common == (get_bit(n, i) == most_common))
+                    .collect()
+            }
+        })[0] as usize
+    }
 }
 
 impl Day<'_> for Day03 {
@@ -22,12 +55,12 @@ impl Day<'_> for Day03 {
     fn part_1(&self) -> Self::T1 {
         let number_counts = self.numbers.len();
         let gamma = (0..self.bit_count)
-            .filter(|i| self.numbers.iter().filter(|&&n| n & (1 << i) > 0).count() > number_counts / 2)
+            .filter(|&i| count_ones(&self.numbers, i) > number_counts / 2)
             .fold(0, |gamma, i| gamma | (1 << i));
         gamma * (!gamma & ((1 << self.bit_count) - 1))
     }
 
     fn part_2(&self) -> Self::T2 {
-        0
+        self.find_by_bit_criteria(true) * self.find_by_bit_criteria(false)
     }
 }
