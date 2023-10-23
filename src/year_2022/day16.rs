5eece19325e18737eb92574b7681d8a1461d10da
Virgile Theuerlacher <vtheuerlacher@posteo.net>
2023-10-23 08:38:51 +0200

    2022 Day 16

--- src/year_2022/day16.rs
+++ src/year_2022/day16.rs
@@ -1,30 +1,102 @@
-use crate::day::Day;
-use fnv::FnvHashMap;
-use regex::Regex;
 use std::borrow::Borrow;
-use std::cell::RefCell;
+use std::cmp::{min, min_by_key};
+use std::thread::current;
 
-pub struct Day16<'a> {
-    valves: FnvHashMap<&'a str, (usize, Vec<(&'a str, usize)>)>,
+use fnv::{FnvHashMap, FnvHashSet};
+use regex::Regex;
+
+use crate::day::Day;
+
+pub struct Day16 {
+    rates: Vec<(usize, usize)>,
+    distances: Vec<Vec<usize>>,
 }
 
-impl<'a> Day<'a> for Day16<'a> {
+fn distances(from: usize, valves_with_flow: &Vec<(usize, usize)>, connections: &Vec<Vec<usize>>) -> Vec<usize> {
+    let (initial_index, _) = valves_with_flow[from];
+    let mut distances = vec![usize::MAX; connections.len()];
+    distances[initial_index] = 0;
+    let mut queue = (0..connections.len()).collect::<Vec<_>>();
+
+    while !queue.is_empty() {
+        let index_in_queue = queue
+            .iter()
+            .enumerate()
+            .min_by_key(|&(_, &v)| distances[v])
+            .map(|(i, _)| i)
+            .unwrap();
+        let valve = queue[index_in_queue];
+        let distance = distances[valve];
+        queue.remove(index_in_queue);
+
+        for &next in &connections[valve] {
+            distances[next] = distances[next].min(distance + 1);
+        }
+    }
+
+    valves_with_flow.iter().map(|&(i, _)| distances[i]).collect()
+}
+
+impl Day16 {
+    fn part_1(&self, remaining: usize, current: usize, visited: u128, score: usize) -> usize {
+        self.distances[current]
+            .iter()
+            .enumerate()
+            .filter(|&(_, &distance)| distance + 1 < remaining)
+            .filter(|&(next, _)| visited & (1 << next) == 0)
+            .map(|(next, &distance)| {
+                let new_remaining = remaining - distance - 1;
+                self.part_1(
+                    new_remaining,
+                    next,
+                    visited | (1 << next),
+                    score + new_remaining * self.rates[next].1,
+                )
+            })
+            .max()
+            .unwrap_or(score)
+    }
+
+    fn part_2(
+        &self,
+        remaining: usize,
+        current: usize,
+        visited: u128,
+        score: usize,
+        path: Vec<usize>,
+    ) -> (usize, Vec<usize>) {
+        self.distances[current]
+            .iter()
+            .enumerate()
+            .filter(|&(_, &distance)| distance + 1 < remaining)
+            .filter(|&(next, _)| visited & (1 << next) == 0)
+            .map(|(next, &distance)| {
+                let new_remaining = remaining - distance - 1;
+                self.part_2(
+                    new_remaining,
+                    next,
+                    visited | (1 << next),
+                    score + new_remaining * self.rates[next].1,
+                    path.iter().copied().chain([next]).collect(),
+                )
+            })
+            .max_by_key(|&(score, _)| score)
+            .unwrap_or((score, path))
+    }
+}
+
+impl Day<'_> for Day16 {
     type T1 = usize;
     type T2 = usize;
 
-    fn new(input: &'a str) -> Self {
+    fn new(input: &str) -> Self {
         let re = Regex::new("^Valve (..) has flow rate=(\\d+); tunnels? leads? to valves? (.+)$").unwrap();
-        let mut valves = FnvHashMap::from_iter(
+        let valves = FnvHashMap::from_iter(
             input
                 .lines()
                 .map_while(|l| {
                     let captures = re.captures(l)?;
-                    let mut next = captures
-                        .get(3)?
-                        .as_str()
-                        .split(", ")
-                        .map(|n| (n, 1))
-                        .collect::<Vec<_>>();
+                    let mut next = captures.get(3)?.as_str().split(", ").collect::<Vec<_>>();
                     next.sort();
                     Some((
                         captures.get(1)?.as_str(),
@@ -32,48 +104,46 @@ impl<'a> Day<'a> for Day16<'a> {
                         next,
                     ))
                 })
-                .map(|(valve, rate, next)| (valve, (rate, RefCell::new(next)))),
+                .map(|(valve, rate, next)| (valve, (rate, next))),
         );
+        let mut names = valves.keys().copied().collect::<Vec<_>>();
+        names.sort();
 
-        while let Some(valve) = valves
+        let valves_with_flow = names
             .iter()
-            .find(|&(&valve, c)| "AA" != valve && c.borrow().0 == 0)
-            .map(|(&valve, _)| valve)
-        {
-            let next = valves[valve].1.borrow().iter().copied().collect::<Vec<_>>();
-            let (left, cost_to_left) = next[0];
-            let (right, cost_to_right) = next[1];
-            valves.remove(valve);
-            valves.entry(left).and_modify(|n| {
-                let vec = n.1.borrow();
-                let mut vec_mut = n.1.borrow_mut();
-                vec_mut.remove(vec.binary_search_by_key(&valve, |(v, _)| *v).unwrap());
-                vec_mut.insert(
-                    vec.binary_search_by_key(&right, |(v, _)| *v).unwrap_err(),
-                    (right, cost_to_right + 1),
-                );
-            });
-            valves.entry(right).and_modify(|n| {
-                let vec = n.1.borrow();
-                let mut vec_mut = n.1.borrow_mut();
-                vec_mut.remove(vec.binary_search_by_key(&valve, |(v, _)| *v).unwrap());
-                vec_mut.insert(
-                    vec.binary_search_by_key(&left, |(v, _)| *v).unwrap_err(),
-                    (left, cost_to_left + 1),
-                );
-            });
-        }
+            .map(|&valve| valves[valve].0)
+            .enumerate()
+            .filter(|&(i, rate)| i == 0 || rate > 0)
+            .collect::<Vec<_>>();
+
+        let connections = names
+            .iter()
+            .map(|&valve| {
+                valves[valve]
+                    .1
+                    .iter()
+                    .map(|v| names.binary_search(v).unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
 
         Self {
-            valves: FnvHashMap::from_iter(valves.into_iter().map(|(k, (r, n))| (k, (r, n.take())))),
+            rates: valves_with_flow.clone(),
+            distances: (0..valves_with_flow.len())
+                .map(|from| distances(from, &valves_with_flow, &connections))
+                .collect(),
         }
     }
 
     fn part_1(&self) -> Self::T1 {
-        0
+        self.part_1(30, 0, 0, 0)
     }
 
     fn part_2(&self) -> Self::T2 {
-        0
+        let (score, visited) = self.part_2(26, 0, 0, 0, vec![]);
+        score
+            + self
+                .part_2(26, 0, visited.into_iter().fold(0, |v, i| v | (1 << i)), 0, vec![])
+                .0
     }
 }
