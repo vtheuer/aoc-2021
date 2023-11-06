ffa6801b8764bdd08a4d3c730b6d47280fd8fcb4
Virgile Theuerlacher <vtheuerlacher@posteo.net>
2023-11-06 09:12:27 +0100

    2022 day 19

--- src/year_2022/day19.rs
+++ src/year_2022/day19.rs
@@ -1,27 +1,27 @@
 use regex::Regex;
-use std::collections::VecDeque;
+
+use Material::*;
 
 use crate::day::Day;
-use crate::year_2022::day19::Material::Ore;
 
 #[derive(Debug, Clone)]
 struct Blueprint {
-    ore_cost: usize,
-    clay_cost: usize,
-    obsidian_cost: (usize, usize),
-    geode_cost: (usize, usize),
+    ore_cost: u8,
+    clay_cost: u8,
+    obsidian_cost: (u8, u8),
+    geode_cost: (u8, u8),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct State {
-    time: usize,
-    ore: (usize, usize),
-    clay: (usize, usize),
-    obsidian: (usize, usize),
-    geode: (usize, usize),
+    time: u8,
+    ore: (u8, u8),
+    clay: (u8, u8),
+    obsidian: (u8, u8),
+    geode: (u8, u8),
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 enum Material {
     Ore,
     Clay,
@@ -29,167 +29,150 @@ enum Material {
     Geode,
 }
 
-use Material::*;
-
 impl State {
-    fn can_build_bot(&self, b: &Blueprint, m: Material) -> bool {
+    fn bots(&self, m: Material) -> u8 {
         match m {
-            Ore => self.ore.1 >= b.ore_cost,
-            Clay => self.ore.1 >= b.clay_cost,
-            Obsidian => self.ore.1 >= b.obsidian_cost.0 && self.clay.1 >= b.obsidian_cost.1,
-            Geode => self.ore.1 >= b.geode_cost.0 && self.obsidian.1 >= b.geode_cost.1,
+            Ore => self.ore.0,
+            Clay => self.clay.0,
+            Obsidian => self.obsidian.0,
+            Geode => self.geode.0,
         }
     }
 
-    fn build_bot(&mut self, b: &Blueprint, m: Material) {
+    fn stock(&self, m: Material) -> u8 {
+        match m {
+            Ore => self.ore.1,
+            Clay => self.clay.1,
+            Obsidian => self.obsidian.1,
+            Geode => self.geode.1,
+        }
+    }
+
+    fn should_build_bot(&self, blueprint: &Blueprint, max_ore_cost: u8, material: Material) -> bool {
+        match material {
+            Ore => self.bots(Ore) < max_ore_cost,
+            Clay => self.bots(Clay) < blueprint.obsidian_cost.1,
+            Obsidian => self.bots(Obsidian) < blueprint.geode_cost.1,
+            Geode => true,
+        }
+    }
+
+    fn build_bot(&self, b: &Blueprint, duration: u8, m: Material) -> Option<State> {
+        let time = 1 + match m {
+            Ore => self.time_to_have_enough(Ore, b.ore_cost)?,
+            Clay => self.time_to_have_enough(Ore, b.clay_cost)?,
+            Obsidian => self
+                .time_to_have_enough(Ore, b.obsidian_cost.0)?
+                .max(self.time_to_have_enough(Clay, b.obsidian_cost.1)?),
+            Geode => self
+                .time_to_have_enough(Ore, b.geode_cost.0)?
+                .max(self.time_to_have_enough(Obsidian, b.geode_cost.1)?),
+        };
+
+        if self.time + time > duration {
+            return None;
+        }
+
+        let mut s = self.advance_by(time);
         match m {
             Ore => {
-                self.ore.0 += 1;
-                self.ore.1 -= b.ore_cost;
+                s.ore.0 += 1;
+                s.ore.1 -= b.ore_cost;
             }
             Clay => {
-                self.clay.0 += 1;
-                self.ore.1 -= b.clay_cost;
+                s.clay.0 += 1;
+                s.ore.1 -= b.clay_cost;
             }
             Obsidian => {
-                self.obsidian.0 += 1;
-                self.ore.1 -= b.obsidian_cost.0;
-                self.clay.1 -= b.obsidian_cost.1;
+                s.obsidian.0 += 1;
+                s.ore.1 -= b.obsidian_cost.0;
+                s.clay.1 -= b.obsidian_cost.1;
             }
             Geode => {
-                self.geode.0 += 1;
-                self.ore.1 -= b.geode_cost.0;
-                self.obsidian.1 -= b.geode_cost.1;
+                s.geode.0 += 1;
+                s.ore.1 -= b.geode_cost.0;
+                s.obsidian.1 -= b.geode_cost.1;
+            }
+        };
+        Some(s)
+    }
+
+    fn time_to_have_enough(&self, material: Material, cost: u8) -> Option<u8> {
+        let stock = self.stock(material);
+        if stock >= cost {
+            Some(0)
+        } else {
+            let bots = self.bots(material);
+            if bots == 0 {
+                None
+            } else {
+                Some((cost - stock).div_ceil(bots))
             }
         }
     }
 
-    fn advance(&mut self) {
-        self.ore.1 += self.ore.0;
-        self.clay.1 += self.clay.0;
-        self.obsidian.1 += self.obsidian.0;
-        self.geode.1 += self.geode.0;
-        self.time += 1;
+    fn advance_by(&self, time: u8) -> Self {
+        let mut s = self.clone();
+        s.time += time;
+        s.ore.1 += time * s.bots(Ore);
+        s.clay.1 += time * s.bots(Clay);
+        s.obsidian.1 += time * s.bots(Obsidian);
+        s.geode.1 += time * s.bots(Geode);
+        s
+    }
+
+    fn score(&self, duration: u8) -> u8 {
+        self.stock(Geode) + self.bots(Geode) * (duration - self.time)
+    }
+
+    fn could_do_better(&self, duration: u8, best: u8) -> bool {
+        let remaining_time = (duration - self.time) as usize;
+
+        // max possible score = stock + bots + (bots + 1) + (bots + 2) + (bots + 3)...
+        //                    = stock + bots + (bots + 1) + (bots + 2) + (bots + 3)...
+        //                    = stock + bots + remaining time * bots + sum(1..=remaining_time)
+        self.stock(Geode) as usize
+            + (remaining_time + 1) * self.bots(Geode) as usize
+            + (remaining_time * (remaining_time + 1)) / 2
+            > best as usize
     }
 }
 
 impl Blueprint {
-    fn score(&self) -> usize {
-        let obsidian_bot_goal = self.geode_cost.1;
-        let clay_bot_goal = self.obsidian_cost.1;
-        let ore_bot_goal =
-            self.geode_cost.0 + self.obsidian_cost.0 * obsidian_bot_goal + self.clay_cost * clay_bot_goal;
-        let mut next_bot_to_build = Clay;
-        let mut state = State {
+    fn score(&self, duration: u8) -> usize {
+        let max_ore_cost = self
+            .ore_cost
+            .max(self.clay_cost)
+            .max(self.obsidian_cost.0)
+            .max(self.geode_cost.0);
+        let mut next = vec![State {
             time: 0,
             ore: (1, 0),
             clay: (0, 0),
             obsidian: (0, 0),
             geode: (0, 0),
-        };
+        }];
+        let mut best = 0;
 
-        while state.time < 24 {
-            if state.can_build_bot(self, next_bot_to_build) {
-                state.build_bot(self, next_bot_to_build);
-                next_bot_to_build = if ore_bot_goal > state.ore.0 {
-                    Ore
-                } else if clay_bot_goal > state.clay.0 {
-                    Clay
-                } else if obsidian_bot_goal > state.obsidian.0 {
-                    Obsidian
-                } else {
-                    Geode
-                }
-            }
-            state.advance();
-            dbg!(&state);
-            dbg!(next_bot_to_build);
-        }
-
-        state.geode.1
-    }
-
-    fn score2(&self) -> usize {
-        let mut max = 0;
-        let mut queue = VecDeque::from([State {
-            time: 0,
-            ore: (1, 0),
-            clay: (0, 0),
-            obsidian: (0, 0),
-            geode: (0, 0),
-        }]);
-        let mut visited = 0;
-
-        while let Some(State {
-            time,
-            ore,
-            clay,
-            obsidian,
-            geode,
-        }) = queue.pop_front()
-        {
-            visited += 1;
-            max = max.max(geode.1);
-            if visited % 100 == 0 {
-                dbg!(visited);
-                dbg!(max);
-                dbg!(queue.len());
-                dbg!(time);
+        while let Some(state) = next.pop() {
+            let score = state.score(duration);
+            if score >= best {
+                best = score;
             }
 
-            if time <= 24 {
-                if ore.1 >= self.geode_cost.0 && obsidian.1 >= self.geode_cost.1 {
-                    queue.push_back(State {
-                        time: time + 1,
-                        ore: (ore.0, ore.1 - self.geode_cost.0 + ore.0),
-                        clay: (clay.0, clay.1 + clay.0),
-                        obsidian: (obsidian.0, obsidian.1 - self.geode_cost.1 + obsidian.0),
-                        geode: (geode.0 + 1, geode.1 + geode.0),
-                    });
+            for material in [Ore, Obsidian, Clay, Geode] {
+                if state.should_build_bot(self, max_ore_cost, material) {
+                    if let Some(s) = state
+                        .build_bot(self, duration, material)
+                        .filter(|s| s.could_do_better(duration, best))
+                    {
+                        next.push(s);
+                    }
                 }
-
-                if ore.1 >= self.obsidian_cost.0 && clay.1 >= self.obsidian_cost.1 {
-                    queue.push_back(State {
-                        time: time + 1,
-                        ore: (ore.0, ore.1 - self.obsidian_cost.0 + ore.0),
-                        clay: (clay.0, clay.1 - self.obsidian_cost.1 + clay.0),
-                        obsidian: (obsidian.0 + 1, obsidian.1 + obsidian.0),
-                        geode: (geode.0, geode.1 + geode.0),
-                    });
-                }
-
-                if ore.1 >= self.clay_cost {
-                    queue.push_back(State {
-                        time: time + 1,
-                        ore: (ore.0, ore.1 - self.clay_cost + ore.0),
-                        clay: (clay.0 + 1, clay.1 + clay.0),
-                        obsidian: (obsidian.0, obsidian.1 + obsidian.0),
-                        geode: (geode.0, geode.1 + geode.0),
-                    });
-                }
-
-                if ore.1 >= self.ore_cost {
-                    queue.push_back(State {
-                        time: time + 1,
-                        ore: (ore.0 + 1, ore.1 - self.ore_cost + ore.0),
-                        clay: (clay.0, clay.1 + clay.0),
-                        obsidian: (obsidian.0, obsidian.1 + obsidian.0),
-                        geode: (geode.0, geode.1 + geode.0),
-                    });
-                }
-
-                queue.push_back(State {
-                    time: time + 1,
-                    ore: (ore.0, ore.1 + ore.0),
-                    clay: (clay.0, clay.1 + clay.0),
-                    obsidian: (obsidian.0, obsidian.1 + obsidian.0),
-                    geode: (geode.0, geode.1 + geode.0),
-                });
             }
         }
-        dbg!(visited);
 
-        max
+        best as usize
     }
 }
 
@@ -224,12 +207,11 @@ impl Day<'_> for Day19 {
         self.blueprints
             .iter()
             .enumerate()
-            .take(1)
-            .map(|(i, b)| (i + 1) * b.score())
+            .map(|(i, b)| (i + 1) * b.score(24))
             .sum()
     }
 
     fn part_2(&self) -> Self::T2 {
-        0
+        self.blueprints.iter().take(3).map(|b| b.score(32)).product()
     }
 }
