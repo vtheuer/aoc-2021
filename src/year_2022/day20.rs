3cc6fbb962dd2cfd6c3df4767d6753139f3812d8
Virgile <virgile.theuerlacher@worldline.com>
2023-11-14 18:14:20 +0100

    2022 Day 20

--- src/year_2022/day20.rs
+++ src/year_2022/day20.rs
@@ -1,157 +1,70 @@
-use std::clone;
-use std::cmp::Ordering;
-use std::collections::VecDeque;
-use std::mem::swap;
-use Ordering::{Equal, Greater, Less};
-
-use fnv::FnvHashMap;
-
 use crate::day::Day;
 
 pub struct Day20 {
     numbers: Vec<isize>,
 }
 
+fn shift(numbers: &mut Vec<(usize, isize)>, index: usize, shift: isize) {
+    let len = numbers.len();
+    let next: Box<dyn Fn(usize) -> usize> = if shift >= 0 {
+        Box::new(|i| if i == len - 1 { 0 } else { i + 1 })
+    } else {
+        Box::new(|i| if i == 0 { len - 1 } else { i - 1 })
+    };
+    let n = numbers[index];
+    let mut i = index;
+    for _ in 0..(shift.abs() % (len as isize - 1)) {
+        let j = next(i);
+        numbers[i] = numbers[j];
+        i = j;
+    }
+    numbers[i] = n;
+}
+
+fn shift_all(nums: &[isize], times: usize) -> isize {
+    let mut numbers = nums.iter().copied().enumerate().collect::<Vec<_>>();
+    let len = numbers.len();
+
+    for _ in 0..times {
+        for i in 0..len {
+            let (current_index, _) = numbers
+                .iter()
+                .enumerate()
+                .find(|&(_, &(initial_index, _))| initial_index == i)
+                .unwrap();
+            let n = numbers[current_index].1;
+            shift(&mut numbers, current_index, n);
+        }
+    }
+
+    let index_of_0 = numbers
+        .iter()
+        .enumerate()
+        .find(|&(_, &(_, v))| v == 0)
+        .map(|(i, _)| i)
+        .unwrap();
+
+    numbers[(index_of_0 + 1000) % len].1 + numbers[(index_of_0 + 2000) % len].1 + numbers[(index_of_0 + 3000) % len].1
+}
+
 impl Day<'_> for Day20 {
-    type T1 = usize;
-    type T2 = usize;
+    type T1 = isize;
+    type T2 = isize;
 
     fn new(input: &str) -> Self {
         Self {
-            numbers: input.lines().map(|l| l.parse().unwrap()).collect(),
+            numbers: input.lines().map_while(|l| l.parse().ok()).collect(),
         }
     }
 
     fn part_1(&self) -> Self::T1 {
-        let mut shifts = vec![0; self.numbers.len()];
-
-        for (i, &n) in self.numbers.iter().enumerate() {
-            let new_i = (i as isize - shifts[i]) as usize;
-            match n.cmp(&0) {
-                Greater => {
-                    let j = new_i + n as usize;
-                    let s = shifts[new_i];
-                    dbg!(i);
-                    dbg!(n);
-                    dbg!(new_i);
-                    dbg!(j);
-                    dbg!(s);
-                    for k in new_i..j {
-                        shifts[k] = shifts[k + 1] + 1;
-                    }
-                    shifts[j] = s - n;
-                }
-                Less => {
-                    if new_i as isize + n >= 0 {
-                        unimplemented!()
-                    } else {
-                        let j = (new_i as isize + shifts.len() as isize - 1 + n) as usize;
-                        let s = shifts[new_i];
-                        dbg!(i);
-                        dbg!(n);
-                        dbg!(new_i);
-                        dbg!(j);
-                        dbg!(s);
-                        for k in new_i..j {
-                            shifts[k] = shifts[k + 1] + 1;
-                        }
-                        shifts[j] = s - (j - new_i) as isize;
-                    }
-                }
-                _ => {}
-            }
-            dbg!(&shifts);
-            dbg!(self.apply(&shifts));
-        }
-
-        0
+        shift_all(&self.numbers, 1)
     }
 
     fn part_2(&self) -> Self::T2 {
-        0
-    }
-}
-
-impl Day20 {
-    fn apply(&self, shifts: &[isize]) -> Vec<isize> {
-        shifts
-            .iter()
-            .enumerate()
-            .map(|(i, &shift)| self.numbers[(i as isize + shift) as usize])
-            .collect()
-    }
-}
-
-fn rotate_right(v: &mut [usize], from: usize, to: usize) {
-    let tmp = v[from];
-
-    for i in (0..(to - from)).rev() {
-        v[(from + i + 1) % v.len()] = v[(from + i) % v.len()]
-    }
-
-    v[to % v.len()] = tmp;
-}
-
-fn shift(current_indices: &mut [usize], values: &[isize], i: usize) {
-    let shift = values[i];
-    let new_index = (i as isize + shift) as usize;
-    // let tmp = current_indices[new_index];
-    // dbg!(i);
-    // dbg!(shift);
-    // dbg!(new_index);
-    // dbg!(tmp);
-
-    // for j in (i..new_index).rev() {
-    //     current_indices[j + 1] = current_indices[j];
-    // }
-    rotate_right(current_indices, i, new_index);
-
-    // current_indices[i] = tmp;
-}
-
-fn shifted(values: &[isize], indices: &[usize]) -> Vec<isize> {
-    let mut shifted = vec![0; values.len()];
-
-    for &i in indices {
-        shifted[indices[i]] = values[i];
-    }
-
-    shifted
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let values = vec![3, 2, 1, 0, -2];
-        let mut current_indices = (0..values.len()).collect::<Vec<_>>();
-        // 01234
-        // 30124
-        // 32014
-        // 32104
-        // 32104
-        // 43102
-
-        shift(&mut current_indices, &values, 0);
-        assert_eq!(current_indices, vec![3, 0, 1, 2, 4]);
-        assert_eq!(shifted(&values, &current_indices), vec![2, 1, 0, 3, -2]);
-
-        shift(&mut current_indices, &values, 1);
-        assert_eq!(current_indices, vec![3, 2, 0, 1, 4]);
-        assert_eq!(shifted(&values, &current_indices), vec![1, 0, 2, 3, -2]);
-
-        shift(&mut current_indices, &values, 2);
-        assert_eq!(current_indices, vec![3, 2, 1, 0, 4]);
-        assert_eq!(shifted(&values, &current_indices), vec![0, 1, 2, 3, -2]);
-
-        shift(&mut current_indices, &values, 3);
-        assert_eq!(current_indices, vec![3, 2, 1, 0, 4]);
-        assert_eq!(shifted(&values, &current_indices), vec![0, 1, 2, 3, -2]);
-
-        shift(&mut current_indices, &values, 4);
-        assert_eq!(current_indices, vec![4, 3, 1, 0, 2]);
-        assert_eq!(shifted(&values, &current_indices), vec![0, 1, -2, 2, 3]);
+        shift_all(
+            &self.numbers.iter().copied().map(|n| n * 811589153).collect::<Vec<_>>(),
+            10,
+        )
     }
 }
